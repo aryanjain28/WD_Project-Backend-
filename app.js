449acafc4705b906942285a1a23fe3b0be61b724
aryanjain28 <aryanjn807@gmail.com>
2023-11-27 10:25:01 -0500

    -> changes in env

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.use("/officeHours", officeHoursRouter);
 // app.use("/customers", customerRouter);
 // app.use("/tasks", taskRouter);
 
-app.listen(3333, () => {
-  console.log(`Server is listening on port: ${3333}.`);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server is listening on port: ${process.env.PORT || 3333}.`);
 });
--- models/db.js
+++ models/db.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 const fs = require("fs");
 
 var connection = mysql.createConnection({
-  host: process.env.HOST,
-  port: process.env.PORT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
